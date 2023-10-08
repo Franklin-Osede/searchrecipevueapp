7d22aea8130a83a78d3e43f75e076b5d5d13e895
Franklin <franklin1014.op@gmail.com>
2023-10-08 20:21:02 +0200

    creating routing navigation menu and giving it styling

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,30 @@
-<template><router-view /></template>
+<template>
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow-md h-16 flex justify-between items-strech">
+      <router-link :to="{ name: 'home' }">Home </router-link>
+      <div class="flex items-center gap-1">
+        <router-link
+          to="/"
+          class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-200"
+          >Search Meals</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-200"
+          >Meals By Letters</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-200"
+          >Meals Ingredients</router-link
+        >
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
 
 <script setup></script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,27 @@
       placeholder="Search for Meals"
     />
     <div class="flex gap-2 justify-center mt-2">
-      <router-link to="/" v-for="letter of letters"> {{ letter }} </router-link>
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
     </div>
   </div>
 </template>
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient";
 
-const meals = computed(() => store.state.meals);
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log("response.data");
+  ingredients.value = response.data;
+});
 </script>
