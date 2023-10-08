8d6a3d38b83fce89bc4fe59647746f8ce01cf8c2
Franklin <franklin1014.op@gmail.com>
2023-10-08 19:17:13 +0200

    link search bar with router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,19 @@
-<template>Home</template>
+<template>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded-border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+    <div class="flex gap-2 justify-center mt-2">
+      <router-link to="/" v-for="letter of letters"> {{ letter }} </router-link>
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+import store from "../store";
 
-<script setup></script>
+const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+</script>
