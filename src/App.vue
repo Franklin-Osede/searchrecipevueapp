1d56f6548e557d2039d1f3788bd932d965df450b
Franklin <franklin1014.op@gmail.com>
2023-10-08 22:11:57 +0200

    created default layouts

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,5 @@
 <template>
-  <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow-md h-16 flex justify-between items-strech">
-      <router-link
-        :to="{ name: 'home' }"
-        class="inline-flex items-center h-full px-5"
-        >Home
-      </router-link>
-      <div class="flex items-center gap-1">
-        <router-link
-          :to="{ name: 'byName' }"
-          class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-100"
-          >Search Meals</router-link
-        >
-        <router-link
-          :to="{ name: 'byLetter' }"
-          class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-100"
-          >Meals By Letters</router-link
-        >
-        <router-link
-          :to="{ name: 'byIngredient' }"
-          class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-100"
-          >Meals Ingredients</router-link
-        >
-      </div>
-    </header>
-    <main>
-      <router-view />
-    </main>
-  </div>
+  <router-view />
 </template>
 
 <script setup></script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-white shadow-md h-16 flex justify-between items-strech">
+    <router-link
+      :to="{ name: 'home' }"
+      class="inline-flex items-center h-full px-5"
+      >Home
+    </router-link>
+    <div class="flex items-center gap-1">
+      <router-link
+        :to="{ name: 'byName' }"
+        class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-100"
+        >Search Meals</router-link
+      >
+      <router-link
+        :to="{ name: 'byLetter' }"
+        class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-100"
+        >Meals By Letters</router-link
+      >
+      <router-link
+        :to="{ name: 'byIngredient' }"
+        class="inline-flex items-center px2 h-full transition-colors hover:bg-purple-100"
+        >Meals Ingredients</router-link
+      >
+    </div>
+  </header>
+</template>
