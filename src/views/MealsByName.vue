6198f475a5052de532c5f35d86b42f45ef763ff6
Franklin <franklin1014.op@gmail.com>
2023-10-08 22:38:39 +0200

    configuring navigation menu

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>By Name</div>
+  <div class="p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded-border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+      @change="searchMeals"
+    />
+
+    <div>
+      <pre> {{ meals }} </pre>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from "@vue/reactivity";
+import { ref } from "vue";
+import axiosClient from "../axiosClient";
+import store from "../store";
+
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
+function searchMeals() {
+  store.dispatch("searchedMeals", keyword.value);
+}
+</script>
