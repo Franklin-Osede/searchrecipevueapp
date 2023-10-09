9dd00d32903a3708fc34a8e9e5c50ddf52c7a8e4
Franklin <franklin1014.op@gmail.com>
2023-10-09 19:57:33 +0200

    syling meals ingredient page

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>By Ingredients</div>
+  <div class="p-8">
+    <div
+      v-for="ingredient of ingredients"
+      :key="ingredient.id"
+      class="bg-white rounded p-3 mb-3 shadow"
+    >
+      <h3 class="text-2xl font-bold">{{ ingredient.strIngredient }}</h3>
+      <p>{{ ingredient.strDescription }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import axiosClient from "../axiosClient";
 
+const ingredients = ref([]);
+
 onMounted(() => {
-  axiosClient.get();
+  axiosClient.get("list.php?i=list").then(({ data }) => {
+    debugger;
+    ingredients.value = data.meals;
+  });
 });
 </script>
